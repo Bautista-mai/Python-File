b550cbe82d67258dd829b0e6532c2ba36ca49326
Bautista-mai <bautistaalma785@gmail.com>
2024-12-03 13:05:31 +0800

    act 1

--- Activity1.py
+++ Activity1.py
@@ -1,2 +1,5 @@
-print("Hello World")
-#iugiu
+print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b"
+            "* * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b* * * * * "
+            "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b\b\b* * * * * * * "
+            "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b* * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b"
+            "* * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t*")
